1d5b8426dfa2b76b9f85307693e0e56e623b6d1d
naripok <fernando.canteruccio@gmail.com>
2020-04-08 21:35:32 -0300

    fix from attribute

--- handler.py
+++ handler.py
@@ -41,16 +41,16 @@ def handler(event, context):
 
     logger.info("keys: {}".format(msg.keys()))
     logger.info("from: {}".format(msg['From']))
-    #original_from = msg['From']
-    #del msg['From']
-    #msg['From'] = re.sub(r'\<.+?\>', '', original_from).strip() + ' <{}>'.format(VERIFIED_FROM_EMAIL)
+    original_from = msg['From']
+    del msg['From']
+    msg['From'] = re.sub(r'\<.+?\>', '', original_from).strip() + ' <{}>'.format(VERIFIED_FROM_EMAIL)
 
     msg['Reply-To'] = VERIFIED_FROM_EMAIL
     msg['Return-Path'] = VERIFIED_FROM_EMAIL
 
     logger.info("subject prefix: {}".format(SUBJECT_PREFIX))
     if SUBJECT_PREFIX and SUBJECT_PREFIX.lower() not in msg.get('Subject').lower():
-        new_subj = ' '.join([SUBJECT_PREFIX, msg.get('Subject', '')])
+        new_subj = ' '.join([SUBJECT_PREFIX, f', from: {original_from}:', msg.get('Subject', '')])
         del msg['Subject']
         msg['Subject'] = new_subj
         logger.info("new subj: {}".format(msg['Subject']))
@@ -70,5 +70,6 @@ def handler(event, context):
             try:
                 o = ses.send_raw_email(Destinations=[address], RawMessage=dict(Data=msg_string))
                 logger.info('Forwarded email for <{}> to <{}>. SendRawEmail response={}'.format(recipient, address, json.dumps(o)))
-            except ClientError as e: logger.error('Client error while forwarding email for <{}> to <{}>: {}'.format(recipient, address, e))
+            except ClientError as e:
+                logger.error('Client error while forwarding email for <{}> to <{}>: {}'.format(recipient, address, e))
 
